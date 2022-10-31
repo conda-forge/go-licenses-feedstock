c885a1ab1b2f2572cfce752b0633de7062b6309d
Uwe L. Korn <uwe.korn@quantco.com>
2022-10-31 22:03:18 +0100

    Allow for native license generation

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,14 @@
 
 go build -v -o $PREFIX/bin/go-licenses
 
-go-licenses save . --save_path=./license-files
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  # Build for osx-64 as this can also be run in cross-compilation mode.
+  GOARCH=amd64 go build -v -o go-licenses-native
+  ./go-licenses-native save . --save_path=./license-files
+else
+  go-licenses save . --save_path=./license-files
+fi
+
 # TODO: remove if not actually needed, see #6
 # rm -r ./license-files/github.com/google/licenseclassifier/licenses
 
