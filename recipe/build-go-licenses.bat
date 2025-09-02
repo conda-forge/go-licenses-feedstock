2dd451d9b29dcb53366c90042cf1430df2ab2c25
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-09-02 09:21:31 -0500

    use explicit exe path on win

--- recipe/build-go-licenses.bat
+++ recipe/build-go-licenses.bat
@@ -2,5 +2,5 @@
 
 go build -v -o "%PREFIX%\bin\go-licenses" ^
     || exit 2
-go-licenses save . --save_path=license-files ^
+"%PREFIX%\bin\go-licenses.exe" save . --save_path=license-files ^
     || exit 3
